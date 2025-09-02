2c32b47952ab36fec919692bd1197c9d15e165b6
Varad Gujarathi <varadgujarathi120@gmail.com>
2025-09-02 22:58:43 +0530

    created the new project folder weather app for understating the use of API and API handling

---
+++ weather-App/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInpt = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+});

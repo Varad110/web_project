7052d7b55e9d25f77b9b36aa85c0725d05a23c1d
Varad Gujarathi <varadgujarathi120@gmail.com>
2025-09-03 23:54:04 +0530

    completed the weather app with the openWeatherMap API and learned how to use the API's into a javascript project and how to handel the api errors

--- weather-App/script.js
+++ weather-App/script.js
@@ -1,4 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cityInpt = document.getElementById("city-input");
   const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-message");
+  const API_KEY = "5318c49aeb027a49d876195310bc3ba1"; //env variables
+  getWeatherBtn.addEventListener("click", async () => {
+    const city = cityInpt.value.trim();
+    if (!city) return;
+
+    //it may throw an error
+    //server/database is always in another continent
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
+  });
+
+  async function fetchWeatherData(city) {
+    //get the data
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("RESPONSE", response);
+
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+    const data = await response.json();
+    return data;
+  }
+
+  function displayWeatherData(data) {
+    console.log(data);
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temperatureDisplay.textContent = `Temperature : ${main.temp}`;
+    descriptionDisplay.textContent = `weather : ${weather[0].description}`;
+
+    //unlock the display
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
+
+  function showError() {
+    //error message
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
 });
